0e1a4713e4ca964239c6b06bc5c4949c7a41c521
BubbaFunnyTime123 <scholkesm@gmail.com>
2026-05-12 18:21:17 -0400

    Changed GHZ and Title Screen Music and Changed GHZ act 1 layout a bit

--- s1.sounddriver.asm
+++ s1.sounddriver.asm
@@ -2636,7 +2636,7 @@ SonicDriverVer = 1 ; Tell SMPS2ASM that we're using Sonic 1's driver.
 ; ---------------------------------------------------------------------------
 ; Music data
 ; ---------------------------------------------------------------------------
-Music81:	include "sound/music/Mus81 - GHZ.asm"
+Music81:	binclude "Splash_Hill_Zone.bin"
 		even
 Music82:	include "sound/music/Mus82 - LZ.asm"
 		even
@@ -2654,7 +2654,7 @@ Music88:	include "sound/music/Mus88 - Extra Life.asm"
 		even
 Music89:	include "sound/music/Mus89 - Special Stage.asm"
 		even
-Music8A:	include "sound/music/Mus8A - Title Screen.asm"
+Music8A:	binclude "GRANDDAD.bin"
 		even
 Music8B:	include "sound/music/Mus8B - Ending.asm"
 		even
